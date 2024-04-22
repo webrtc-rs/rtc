98b4ec4ae015b151355639fcc4067ef77a9f49a8
yngrtc <y@ngr.tc>
2024-04-21 17:31:33 -0700

    Fix simulcast for firefox (#534)

--- rtc/src/constants.rs
+++ rtc/src/constants.rs
@@ -6,8 +6,6 @@ pub(crate) const RECEIVE_MTU: usize = 1460;
 pub(crate) const SDP_ATTRIBUTE_RID: &str = "rid";
 pub(crate) const SDP_ATTRIBUTE_SIMULCAST: &str = "simulcast";
 pub(crate) const GENERATED_CERTIFICATE_ORIGIN: &str = "WebRTC";
-pub(crate) const SDES_REPAIR_RTP_STREAM_ID_URI: &str =
-    "urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id";
 pub(crate) const DEFAULT_SESSION_SRTP_REPLAY_PROTECTION_WINDOW: usize = 64;
 pub(crate) const DEFAULT_SESSION_SRTCP_REPLAY_PROTECTION_WINDOW: usize = 64;
 pub(crate) const DEFAULT_DTLS_REPLAY_PROTECTION_WINDOW: usize = 64;
