b535cbd33e73c4db1cdb2d4053b0d4b315dab211
yngrtc <y@ngr.tc>
2026-02-20 15:40:50 -0800

    update rtc-signal

--- examples/examples/signal/src/lib.rs
+++ examples/examples/signal/src/lib.rs
@@ -88,16 +88,9 @@ pub fn must_read_stdin() -> Result<String> {
     Ok(line)
 }
 
-// Allows compressing offer/answer to bypass terminal input limits.
-// const COMPRESS: bool = false;
-
 /// encode encodes the input in base64
 /// It can optionally zip the input before encoding
 pub fn encode(b: &str) -> String {
-    //if COMPRESS {
-    //    b = zip(b)
-    //}
-
     BASE64_STANDARD.encode(b)
 }
 
@@ -105,50 +98,9 @@ pub fn encode(b: &str) -> String {
 /// It can optionally unzip the input after decoding
 pub fn decode(s: &str) -> Result<String> {
     let b = BASE64_STANDARD.decode(s)?;
-
-    //if COMPRESS {
-    //    b = unzip(b)
-    //}
-
     let s = String::from_utf8(b)?;
     Ok(s)
 }
-/*
-func zip(in []byte) []byte {
-    var b bytes.Buffer
-    gz := gzip.NewWriter(&b)
-    _, err := gz.Write(in)
-    if err != nil {
-        panic(err)
-    }
-    err = gz.Flush()
-    if err != nil {
-        panic(err)
-    }
-    err = gz.Close()
-    if err != nil {
-        panic(err)
-    }
-    return b.Bytes()
-}
-
-func unzip(in []byte) []byte {
-    var b bytes.Buffer
-    _, err := b.Write(in)
-    if err != nil {
-        panic(err)
-    }
-    r, err := gzip.NewReader(&b)
-    if err != nil {
-        panic(err)
-    }
-    res, err := ioutil.ReadAll(r)
-    if err != nil {
-        panic(err)
-    }
-    return res
-}
-*/
 
 pub fn get_local_ip() -> IpAddr {
     if let Ok(socket) = std::net::UdpSocket::bind("0.0.0.0:0")
