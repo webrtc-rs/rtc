f9dfa33e500c8d262d61c3c5019729dc61d240ae
Lann <github-lann@lannbox.com>
2026-07-12 21:40:54 -0400

    Allow targetting wasm32-wasip2 (#126)
    * rtc-shared: Fallback ifaces impl for non-windows non-unix targets
    
    * rtc-mdns: Upgrade socket2 to 0.6

--- rtc-shared/src/ifaces/ffi/mod.rs
+++ rtc-shared/src/ifaces/ffi/mod.rs
@@ -7,3 +7,11 @@ pub use self::windows::ifaces;
 mod unix;
 #[cfg(target_family = "unix")]
 pub use self::unix::ifaces;
+
+#[cfg(not(any(target_family = "windows", target_family = "unix")))]
+pub fn ifaces() -> Result<Vec<super::Interface>, std::io::Error> {
+    Err(std::io::Error::new(
+        std::io::ErrorKind::Unsupported,
+        "ifaces is not supported on this platform",
+    ))
+}
