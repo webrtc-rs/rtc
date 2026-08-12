704808d836dcd262b8dc6a6e21fd63bfff29ec4e
Rain Liu <y@liu.mx>
2026-08-11 23:46:33 -0700

    re-expose RTCIceServerTransportProtocol, RTCIceTcpCandidateType

--- src/peer_connection/transport/mod.rs
+++ src/peer_connection/transport/mod.rs
@@ -167,6 +167,7 @@ use std::fmt;
 pub use ice::candidate::{
     CandidateConfig, CandidateHostConfig, CandidatePeerReflexiveConfig, CandidateRelayConfig,
     CandidateServerReflexiveConfig, RTCIceCandidate, RTCIceCandidateInit,
+    RTCIceServerTransportProtocol, RTCIceTcpCandidateType,
 };
 pub use ice::candidate_pair::RTCIceCandidatePair;
 pub use ice::candidate_type::RTCIceCandidateType;
